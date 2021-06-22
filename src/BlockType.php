57672d0dbb1ba4a78e17bea6e14b9f0d96d18091
Wes Bosman <wes@nudgecoach.com>
2021-06-22 18:22:09 -0400

    Added a block type. Notion is built upon the block type maybe I should start there.

---
+++ src/BlockType.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace WesBosman\LaravelNotion;
+
+class BlockType {
+    const PARAGRAPH = "paragraph";
+    const HEADING_1 = "heading_1";
+    const HEADING_2 = "heading_2";
+    const HEADING_3 = "heading_3";
+    const BULLETED_LIST_ITEM = "bulleted_list_item";
+    const NUMBERED_LIST_ITEM = "numbered_list_item";
+    const TO_DO = "to_do";
+    const TOGGLE = "toggle";
+    const CHILD_PAGE = "child_page";
+    const UNSUPPORTED = "unsupported";
+}
